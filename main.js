31bc9f74885017817b54422ad031d1aaf86fcb0a
MGG <matthieu.gallou.guyot@gmail.com>
2024-08-22 22:09:22 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-document.querySelectorAll('a').forEach(function(link) {
-    if (!link.classList.contains('no-target')) {
+document.querySelectorAll('a[href]').forEach(function(link) {
+    if (link.href.startsWith('https://matthieugg.github.io/')) {
+        // do nothing = same window for internal
+    } else {
+        // add target="_blank" to external
         link.setAttribute('target', '_blank');
     }
 });
