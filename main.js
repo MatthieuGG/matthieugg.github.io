b8307e875829305632fbdeff92f65b9e5286501d
MGG <matthieu.gallou.guyot@gmail.com>
2024-08-24 03:44:59 +0900

    update main.js
    automatic add of nav bar

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+// Automatically add the navigation bar to the body of each page
+document.addEventListener('DOMContentLoaded', function() {
+    const navbarHTML = `
+        <div class="navbar-container">
+            <div class="navbar">
+                <a href="index.html">Home</a>
+                <a href="resume_MGG.pdf">Resume</a>
+                <a href="publications.html">Publications</a>
+                <a href="income.html">INCOME</a>
+            </div>
+        </div>
+    `;
+
+    // Insert the navigation bar at the top of the body
+    document.body.insertAdjacentHTML('afterbegin', navbarHTML);
+});
+
+// Add target="_blank" to external links
 document.querySelectorAll('a[href]').forEach(function(link) {
     if (link.href.startsWith('https://matthieugg.github.io/')) {
         // do nothing = same window for internal
