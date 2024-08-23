05189dd2f79ed87cc8d2d6832fcdc7cda4aab1d4
MGG <matthieu.gallou.guyot@gmail.com>
2024-08-24 04:00:26 +0900

    update resume

--- main.js
+++ main.js
@@ -1,21 +1,18 @@
+// Automatically add the navigation bar to the body of each page
 document.addEventListener('DOMContentLoaded', function() {
-    // Check if the navbar is already present
-    if (!document.querySelector('.navbar-container')) {
-        const navbarHTML = `
-            <div class="navbar-container">
-                <div class="navbar">
-                    <a href="index.html">Home</a>
-                    <a href="resume_MGG.pdf">Resume</a>
-                    <a href="publications.html">Publications</a>
-                    <a href="income.html">INCOME</a>
-                    <!-- <a href="slowlife.html">Slow Life</a> -->
-                </div>
+    const navbarHTML = `
+        <div class="navbar-container">
+            <div class="navbar">
+                <a href="index.html">Home</a>
+                <a href="resume_MGG.pdf">Resume</a>
+                <a href="publications.html">Publications</a>
+                <a href="income.html">INCOME</a>
             </div>
-        `;
+        </div>
+    `;
 
-        // Insert the navigation bar at the top of the body
-        document.body.insertAdjacentHTML('afterbegin', navbarHTML);
-    }
+    // Insert the navigation bar at the top of the body
+    document.body.insertAdjacentHTML('afterbegin', navbarHTML);
 });
 
 // Add target="_blank" to external links
