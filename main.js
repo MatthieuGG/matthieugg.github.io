bd522e6fef7b1ae9a313ae48f152fe8eec018b45
MGG <matthieu.gallou.guyot@gmail.com>
2024-12-17 21:31:54 +0900

    update streamlit
    relete /~/+/

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 <a href="resume_MGG.pdf">Resume</a>
                 <a href="productions.html">Productions</a>
                 <a href="income.html">INCOME</a>
-                <a href="https://dualtaskcalculator.streamlit.app/~/+/" class="highlighted-link">[DualTaskCalculator]</a>
+                <a href="https://dualtaskcalculator.streamlit.app/" class="highlighted-link">[DualTaskCalculator]</a>
             </div>
         </div>
     `;
