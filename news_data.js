c13946dc7a847413fa8945efc6092da6cf8c6bef
Matthieu Gallou-Guyot <matthieu.gallou.guyot@gmail.com>
2025-07-30 18:19:00 +0900

    typos

--- news_data.js
+++ news_data.js
@@ -2,17 +2,17 @@
 const newsEntries = [
   {
     date: `2025-07-29`,
-    title: `Lauching the "News"!`,
+    title: `Launching the "News"!`, // 
     content: `
       <p>New section on this website! 
-      I'll post here science-related news: research, clinic, teaching, congress, projects, social events, etc. 
-      I also took the opportunity to refresh the website, including the "home" presentation, and the implementation of a 
+      I'll post here science-related news: research, clinic, teaching, congresses, projects, social events, etc. 
+      I also took the opportunity to refresh the website, including the "home" presentation and the implementation of a 
       <a href="https://matthieugg.github.io/resume.html">dynamic PDF reader</a> for my (updated) resume.</p>
 
-      <p>No "likes", no react, no comments, just a specific oriented information diffusion. 
+      <p>No "likes", no reactions, no comments — just purposefully oriented information diffusion. 
       So little that no newsletter would make sense, at least for now. 
       Inspired by <a href="https://calnewport.com/writing/">digital minimalism</a>, with text and few media.  
-      A good start for decentralised internet, using less 
+      A good start for a decentralised internet — using less 
       <a href="https://linkedin.com/in/matthieu-gallou-guyot">LinkedIn</a> and 
       <a href="https://jp.linkedin.com/in/matthieu-gallou-guyot">ResearchGate</a>, maybe?</p>
     `
