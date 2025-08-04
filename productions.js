cbaebb73214c6cefba3e4534f5c1fd06e19e0336
Matthieu Gallou-Guyot <matthieu.gallou.guyot@gmail.com>
2025-08-04 15:34:22 +0900

    maj

--- productions.js
+++ productions.js
@@ -2,9 +2,27 @@ const productionsPerPage = 5;
 let currentPage = 1;
 let currentYear = "all";
 
+// Renommer pour correspondre à ta data
+const productions = productionsEntries;
+
 function getFilteredProductions() {
-  if (currentYear === "all") return productions;
-  return productions.filter(p => p.year === currentYear);
+  if (currentYear === "all") {
+    // Flatten all entries across years
+    return productions.flatMap(yearObj =>
+      yearObj.entries.map(entry => ({
+        year: yearObj.year,
+        ...entry
+      }))
+    );
+  } else {
+    // Trouver l'année choisie et retourner ses entrées
+    const yearObj = productions.find(p => p.year == currentYear);
+    if (!yearObj) return [];
+    return yearObj.entries.map(entry => ({
+      year: yearObj.year,
+      ...entry
+    }));
+  }
 }
 
 function displayProductions(page) {
@@ -18,14 +36,15 @@ function displayProductions(page) {
 
   pageItems.forEach((item) => {
     const div = document.createElement("div");
-    div.className = "production-highlight";
+    div.className = "article-highlight"; // pour utiliser ton style image + texte
+
     div.innerHTML = `
-      <div class="production-image">
-        <img src="${item.image}" alt="${item.title}">
+      <div class="article-image">
+        <img src="${item.img}" alt="${item.alt}">
       </div>
-      <div class="production-text">
-        <strong>${item.year} – ${item.title}</strong><br>
-        ${item.description}
+      <div class="article-text">
+        <strong>${item.year}</strong><br>
+        ${item.text || `<a href="${item.href}" target="_blank" rel="noopener noreferrer">${item.alt}</a>`}
       </div>
     `;
     container.appendChild(div);
@@ -72,7 +91,7 @@ function setupYearFilter() {
   years.forEach(year => {
     const btn = document.createElement("button");
     btn.textContent = year;
-    if (year === currentYear) btn.className = "active";
+    if (year == currentYear) btn.className = "active";
     btn.addEventListener("click", () => {
       currentYear = year;
       currentPage = 1;
--- productions_data.js
+++ productions_data.js
@@ -1,114 +1,321 @@
-const productions = [
+// productions_data.js
+const productionsEntries = [
   {
-    year: "2025",
-    image: "img/carousel/2025-jmpb.png",
-    title: "ActiWearCheck",
-    description: `Published in <em>Journal for Measurement in Physical Behaviour</em>.<br>
-      <a href="https://journals.humankinetics.com/view/journals/jmpb/8/1/article-jmpb.2024-0053.xml">Read article</a>`
+    year: 2025,
+    title: "2025",
+    entries: [
+      {
+        href: "https://journals.humankinetics.com/view/journals/jmpb/8/1/article-jmpb.2024-0053.xml",
+        img: "img/carousel/2025-jmpb.png",
+        alt: "ActiWearCheck",
+        text: ""
+      },
+      {
+        href: "https://sportrxiv.org/index.php/server/preprint/view/550/version/705",
+        img: "img/carousel/2025-sportrxiv.jpg",
+        alt: "sportXRiv",
+        text: ""
+      },
+      {
+        href: "https://onlinelibrary.wiley.com/doi/full/10.1111/bjh.20150",
+        img: "img/carousel/2025-bjh.jpg",
+        alt: "drePAnon 1",
+        text: ""
+      },
+      {
+        href: "https://rehab-journal.com/index.php/home/article/view/58",
+        img: "img/carousel/2025-erj.png",
+        alt: "DTP",
+        text: ""
+      },
+      {
+        href: "https://github.com/OchaUni-Physical-Activity-Measurement/ActiWearCheck",
+        img: "img/carousel/2024-actiwear-b.png",
+        alt: "ActiWearCheck",
+        text: ""
+      }
+    ]
   },
   {
-    year: "2025",
-    image: "img/carousel/2025-sportrxiv.jpg",
-    title: "Preprint on SportRxiv",
-    description: `An open preprint exploring physical activity monitoring.<br>
-      <a href="https://sportrxiv.org/index.php/server/preprint/view/550/version/705">Read preprint</a>`
+    year: 2024,
+    title: "2024",
+    entries: [
+      {
+        href: "https://www.sciencedirect.com/science/article/pii/S1568163724002769?via%3Dihub",
+        img: "img/carousel/2024-arr.png",
+        alt: "10.1016/j.arr.2024.102458",
+        text: ""
+      },
+      {
+        href: "https://rehab-journal.com/index.php/home/article/view/46",
+        img: "img/carousel/2024-erj.jpg",
+        alt: "10.52057/erj.v4i1.46",
+        text: ""
+      },
+      {
+        href: "https://www.sciencedirect.com/science/article/pii/S1779012323003315",
+        img: "img/carousel/2024-klr.jpg",
+        alt: "10.1016/j.kine.2023.11.004",
+        text: ""
+      },
+      {
+        href: "https://www.em-consulte.com/article/1624922/figures/soutien-ou-maintien-a-domicile-des-personnes-agees",
+        img: "img/carousel/2024-emc.jpg",
+        alt: "10.1016/S1283-0887(23)69392-2",
+        text: ""
+      }
+    ]
   },
   {
-    year: "2025",
-    image: "img/carousel/2025-bjh.jpg",
-    title: "drePAnon Study",
-    description: `Published in <em>British Journal of Haematology</em>.<br>
-      <a href="https://onlinelibrary.wiley.com/doi/full/10.1111/bjh.20150">Read article</a>`
+    year: 2023,
+    title: "2023",
+    entries: [
+      {
+        href: "https://doi.org/10.20693/jspehssconf.73.0_463",
+        img: "img/carousel/2023-jspehs.jpg",
+        alt: "10.20693/jspehssconf.73.0_463",
+        text: ""
+      },
+      {
+        href: "",
+        img: "img/carousel/2023-jsps.jpg",
+        alt: "JSPS",
+        text: ""
+      },
+      {
+        href: "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC10360332/",
+        img: "img/carousel/2023-bmc.jpg",
+        alt: "10.1186/s12906-023-04074-w",
+        text: ""
+      },
+      {
+        href: "https://github.com/MatthieuGG/GPAQ-scores",
+        img: "img/carousel/2023-gpaq-b.png",
+        alt: "10.5281/zenodo.10086826",
+        text: ""
+      },
+      {
+        href: "https://github.com/MatthieuGG/SF36-scores",
+        img: "img/carousel/2023-sf36-b.png",
+        alt: "10.5281/zenodo.10086861",
+        text: ""
+      },
+      {
+        href: "https://www.sciencedirect.com/science/article/pii/S177901232200537X",
+        img: "img/carousel/2023-klr.jpg",
+        alt: "10.1016/j.kine.2022.12.176",
+        text: ""
+      },
+      {
+        href: "https://linkinghub.elsevier.com/retrieve/pii/S1779012323000165",
+        img: "img/carousel/2023-klr1.jpg",
+        alt: "10.1016/j.kine.2023.01.004",
+        text: ""
+      },
+      {
+        href: "",
+        img: "img/carousel/2023-qualif.jpg",
+        alt: "Qualification CNU MGG",
+        text: ""
+      },
+      {
+        href: "https://www.frontiersin.org/articles/10.3389/fnagi.2023.1046676",
+        img: "img/carousel/2023-frontiers.jpg",
+        alt: "10.3389/fnagi.2023.1046676",
+        text: ""
+      },
+      {
+        href: "https://games.jmir.org/2023/1/e36126",
+        img: "img/carousel/2023-jmir.jpg",
+        alt: "10.2196/36126",
+        text: ""
+      }
+    ]
   },
   {
-    year: "2025",
-    image: "img/carousel/2025-erj.png",
-    title: "DTP publication",
-    description: `Published in <em>European Rehabilitation Journal</em>.<br>
-      <a href="https://rehab-journal.com/index.php/home/article/view/58">Read article</a>`
+    year: 2022,
+    title: "2022",
+    entries: [
+      {
+        href: "https://tel.archives-ouvertes.fr/tel-03715392",
+        img: "img/carousel/2023-thesis.jpg",
+        alt: "Thesis MGG PhD",
+        text: ""
+      },
+      {
+        href: "https://osf.io/kt54b/",
+        img: "img/carousel/2022-osf.png",
+        alt: "Every INCOME documents",
+        text: ""
+      },
+      {
+        href: "",
+        img: "img/carousel/2022-isprm.jpg",
+        alt: "10.4103/2349-7904.351394",
+        text: ""
+      },
+      {
+        href: "",
+        img: "img/carousel/2022-isprm2.png",
+        alt: "10.4103/2349-7904.351394",
+        text: ""
+      },
+      {
+        href: "https://rehab-journal.com/index.php/home/article/view/8",
+        img: "img/carousel/2022-erj.jpg",
+        alt: "10.52057/erj.v2i1.8",
+        text: ""
+      },
+      {
+        href: "https://www.tandfonline.com/doi/full/10.1080/09638288.2021.2022780",
+        img: "img/carousel/2022-dr.jpg",
+        alt: "10.1080/09638288.2021.2022780",
+        text: ""
+      },
+      {
+        href: "https://www.elsevier-masson.fr/reeducation-en-neurologie-9782294770524.html",
+        img: "img/carousel/2022-elsevier.jpg",
+        alt: "978-2-294-77123-1",
+        text: ""
+      },
+      {
+        href: "",
+        img: "img/carousel/2022-news.jpg",
+        alt: "Newsletter",
+        text: ""
+      }
+    ]
   },
   {
-    year: "2024",
-    image: "img/carousel/2024-arr.png",
-    title: "Aging Research Review",
-    description: `<a href="https://www.sciencedirect.com/science/article/pii/S1568163724002769?via%3Dihub">Read article</a>`
+    year: 2021,
+    title: "2021",
+    entries: [
+      {
+        href: "https://osf.io/csdz9",
+        img: "img/carousel/2021-program.jpg",
+        alt: "INCOME training program",
+        text: ""
+      },
+      {
+        href: "https://osf.io/6sc5a",
+        img: "img/carousel/2021-installation.jpg",
+        alt: "INCOME exergame installation",
+        text: ""
+      },
+      {
+        href: "https://osf.io/wp5v8",
+        img: "img/carousel/2021-cpp.jpg",
+        alt: "INCOME ethic",
+        text: ""
+      },
+      {
+        href: "https://www.unilim.fr/trahs/3945",
+        img: "img/carousel/2021-trash.jpg",
+        alt: "10.25965/TRAHS.3945",
+        text: ""
+      },
+      {
+        href: "https://linkinghub.elsevier.com/retrieve/pii/S2211034821001954",
+        img: "img/carousel/2021-ms.jpg",
+        alt: "10.1016/j.msard.2021.102928",
+        text: ""
+      },
+      {
+        href: "https://linkinghub.elsevier.com/retrieve/pii/S016643282030680X",
+        img: "img/carousel/2020-bbr.jpg",
+        alt: "10.1016/j.bbr.2020.112981",
+        text: ""
+      }
+    ]
   },
   {
-    year: "2024",
-    image: "img/carousel/2024-erj.jpg",
-    title: "European Rehabilitation Journal",
-    description: `<a href="https://rehab-journal.com/index.php/home/article/view/46">Read article</a>`
+    year: 2020,
+    title: "2020",
+    entries: [
+      {
+        href: "http://www.sciencedirect.com/science/article/pii/S0987705320301209",
+        img: "img/carousel/2020-nccn.jpg",
+        alt: "10.1016/j.neucli.2020.10.010",
+        text: ""
+      },
+      {
+        href: "http://www.sciencedirect.com/science/article/pii/S1568163720302701",
+        img: "img/carousel/2020-arr.jpg",
+        alt: "10.1016/j.arr.2020.101135",
+        text: ""
+      },
+      {
+        href: "http://www.sciencedirect.com/science/article/pii/S016801022030420X",
+        img: "img/carousel/2020-nr.jpg",
+        alt: "10.1016/j.neures.2020.07.012",
+        text: ""
+      },
+      {
+        href: "https://www.limoges.fr/citoyenne/magazine-vivre-limoges",
+        img: "img/carousel/2020-val.png",
+        alt: "Vivre a Limoges",
+        text: ""
+      }
+    ]
   },
   {
-    year: "2024",
-    image: "img/carousel/2024-klr.jpg",
-    title: "Kiné la Revue",
-    description: `<a href="https://www.sciencedirect.com/science/article/pii/S1779012323003315">Read article</a>`
+    year: "2019",
+    title: "2019",
+    entries: [
+      {
+        href: "https://linkinghub.elsevier.com/retrieve/pii/S0987705319302679",
+        img: "img/carousel/2019-sofpel.jpg",
+        alt: "10.1016/j.neucli.2019.10.045",
+        text: ""
+      },
+      {
+        href: "https://linkinghub.elsevier.com/retrieve/pii/S0987705319303041",
+        img: "img/carousel/2019-sofpel1.jpg",
+        alt: "10.1016/j.neucli.2019.10.082",
+        text: ""
+      }
+    ]
   },
   {
-    year: "2024",
-    image: "img/carousel/2024-emc.jpg",
-    title: "Domicile des personnes âgées",
-    description: `<a href="https://www.em-consulte.com/article/1624922/figures/soutien-ou-maintien-a-domicile-des-personnes-agees">Read article</a>`
+    year: "2018",
+    title: "2018",
+    entries: [
+      {
+        href: "",
+        img: "img/carousel/2018-thesis.jpg",
+        alt: "MGG thesis M2",
+        text: ""
+      }
+    ]
   },
   {
-    year: "2023",
-    image: "img/carousel/2023-jspehs.jpg",
-    title: "JSPEHSS Conference",
-    description: `<a href="https://doi.org/10.20693/jspehssconf.73.0_463">Read abstract</a>`
+    year: "2017",
+    title: "2017",
+    entries: [  
+      {
+        href: "",
+        img: "img/carousel/2017-thesis.jpg",
+        alt: "MGG thesis M1",
+        text: ""
+      },
+      {
+        href: "https://linkinghub.elsevier.com/retrieve/pii/S1779012317301274",
+        img: "img/carousel/2017-jfk.jpg",
+        alt: "10.1016/j.kine.2017.02.066",
+        text: ""
+      }
+    ]
   },
   {
-    year: "2023",
-    image: "img/carousel/2023-bmc.jpg",
-    title: "BMC CAM article",
-    description: `<a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC10360332/">Read article</a>`
-  },
-  {
-    year: "2023",
-    image: "img/carousel/2023-gpaq-b.png",
-    title: "GPAQ scores",
-    description: `<a href="https://github.com/MatthieuGG/GPAQ-scores">See GitHub repo</a>`
-  },
-  {
-    year: "2023",
-    image: "img/carousel/2023-sf36-b.png",
-    title: "SF-36 scores",
-    description: `<a href="https://github.com/MatthieuGG/SF36-scores">See GitHub repo</a>`
-  },
-  {
-    year: "2023",
-    image: "img/carousel/2023-jmir.jpg",
-    title: "JMIR Serious Games",
-    description: `<a href="https://games.jmir.org/2023/1/e36126">Read article</a>`
-  },
-  {
-    year: "2022",
-    image: "img/carousel/2022-erj.jpg",
-    title: "European Rehabilitation Journal",
-    description: `<a href="https://rehab-journal.com/index.php/home/article/view/8">Read article</a>`
-  },
-  {
-    year: "2022",
-    image: "img/carousel/2022-dr.jpg",
-    title: "Disability & Rehabilitation",
-    description: `<a href="https://www.tandfonline.com/doi/full/10.1080/09638288.2021.2022780">Read article</a>`
-  },
-  {
-    year: "2022",
-    image: "img/carousel/2022-elsevier.jpg",
-    title: "Neurology Textbook Chapter",
-    description: `<a href="https://www.elsevier-masson.fr/reeducation-en-neurologie-9782294770524.html">See book</a>`
-  },
-  {
-    year: "2021",
-    image: "img/carousel/2021-ms.jpg",
-    title: "MSARD Article",
-    description: `<a href="https://linkinghub.elsevier.com/retrieve/pii/S2211034821001954">Read article</a>`
-  },
-  {
-    year: "2020",
-    image: "img/carousel/2020-arr.jpg",
-    title: "Aging Research Reviews",
-    description: `<a href="http://www.sciencedirect.com/science/article/pii/S1568163720302701">Read article</a>`
+    year: "2016",
+    title: "2016",
+    entries: [  
+      {
+        href: "",
+        img: "img/carousel/2016-thesis.jpg",
+        alt: "MGG thesis MKDE",
+        text: ""
+      }
+    ]
   }
 ];
