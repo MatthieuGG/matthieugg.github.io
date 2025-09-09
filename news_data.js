750ce8df2278ba2cdd9f595b1b444ec4baf2c784
Matthieu Gallou-Guyot <matthieu.gallou.guyot@gmail.com>
2025-09-09 14:02:33 +0900

    news Osaka

--- news_data.js
+++ news_data.js
@@ -1,6 +1,28 @@
 // news_data.js
 const newsEntries = [
   {
+    date: `2025-09-09`,
+    title: `Congrès à Osaka, et passage en français`, 
+    image: "img/news/2024_oasaka.jpg",  
+    content: 
+    `
+      <p>
+      En ce début de mois de septembre, et malgré la chaleur japonaise écrasante, je suis pour la semaine 
+      en congrès à Osaka. Au-delà de la chance de voyager, c'est l'opportunité pour moi de présenter mes 
+      premiers travaux de neurosciences « pures ». C'est-à-dire incluant de belles images de cerveau 
+      (<a href="https://www.sciencedirect.com/science/article/abs/pii/S0010027707002053?via%3Dihub">attention aux biais</a>).  
+      Le poster dans son ensemble est consultable 
+      <a href="https://drive.google.com/file/d/1oH6xeO38jlgjcyndYTl1tHBvIRZNTaB-/view?usp=drive_link">ici</a>.
+      </p>
+      
+
+      <p>
+      Ceci s'accompagne d'un changement majeur, avec le passage de ce site internet en français. Peut-être le signe
+      d'une fin d'expérience internationale, et d'un retour en Europe francophone ? Affaire à suivre.
+      </p>
+    `
+  },
+  {
     date: `2025-07-29`,
     title: `Launching the "News"!`, 
     //image: "img/news/Breaking_News_Alert.png",  
