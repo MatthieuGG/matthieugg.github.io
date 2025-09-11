1d9f1c335e518488a800b71d2693816fb937e571
Matthieu Gallou-Guyot <matthieu.gallou.guyot@gmail.com>
2025-09-11 14:57:52 +0900

    news cinet

--- news_data.js
+++ news_data.js
@@ -1,64 +1,90 @@
 // news_data.js
 const newsEntries = [
-    {
-    date: `2025-09-10`,
-    title: "Journée mondiale de la physiothérapie 2025 : vieillir en santé grâce à l’activité physique", 
-    image: "img/news/2024_hesav.png",  
+  {
+    date: `2025-09-11`,
+    title: "Visite du CiNet à l'Université d'Osaka", 
+    image: "img/news/2025_cinet.jpg",  
     content: 
     `
       <p>
-      Premiere micro-prise de parole a <strong>HESAV</strong> pour la Journée mondiale de la physiothérapie,
+      Actualité chargée en ce début septembre ! À la suite du congrès, j’ai eu la chance de visiter le 
+      <a href="https://cinet.jp/english/">CiNet</a>, un grand centre de 
+      recherche appartenant au NICT, rattaché à l’Université d’Osaka.
+      </p>
+
+      <p>
+      J’ai d’abord eu droit à une visite guidée du centre. Entièrement neuf, quatre IRM 3T, une IRM 7T, un MEG, des EEG, 
+      plus de 200 spécialistes… Magnifique ! Puis nous avons eu une réunion de travail très instructive avec 
+      <a href="https://cinet.jp/english/people/2014465/">Ikuhiro Kida</a>. Beaucoup de travail reste à faire pour 
+      améliorer mon projet de recherche actuel. Espérons que cette collaboration perdure !
+      </p>
+    `
+  },
+  {
+    date: `2025-09-10`,
+    title: "Journée mondiale de la physiothérapie 2025 : vieillir en santé grâce à l’activité physique", 
+    image: "img/news/2025_hesav.png",  
+    content: 
+    `
+      <p>
+      Première micro-prise de parole à <strong>HESAV</strong> pour la Journée mondiale de la physiothérapie,
       consacrée au rôle de la physiothérapie et de l’activité physique dans le vieillissement 
       en bonne santé en 2025.
       <br>
-      J'y soutiens les avantages de recruter des patient.e.s partenaires, d'utiliser des kits de vieillissement, 
-      et de pratiquer sur des bases theoriques solides. Ceci permet de toujours s'adapter, loin des recettes toutes faites.
+      J’y ai défendu les avantages de recruter des patient·e·s partenaires, d’utiliser des kits de vieillissement 
+      et de beaucoup pratiquer, sur des bases théoriques solides. Cela permet de toujours s’adapter, loin des recettes toutes faites.
       </p>
       
       <p>
-      L'article est a retrouver en integralité 
+      L’article est à retrouver dans son intégralité 
       <a href="https://hesav.ch/physiotherapie-vieillir-en-sante/">ici</a>.
       </p>
     `
   },
   {
     date: `2025-09-09`,
-    title: `Congrès à Osaka, et passage en Français`, 
-    image: "img/news/2024_osaka.jpg",  
+    title: `Congrès à Osaka et passage en français`, 
+    image: "img/news/2025_osaka.jpg",  
     content: 
     `
       <p>
-      En ce début de mois de septembre, et malgré la chaleur japonaise écrasante, je suis pour la semaine 
-      en congrès à Osaka. Au-delà de la chance de voyager, c'est l'opportunité pour moi de présenter mes 
-      premiers travaux de neurosciences « pures ». C'est-à-dire incluant de belles images de cerveau 
+      En ce début de mois de septembre, et malgré la chaleur japonaise écrasante, je participe cette semaine 
+      au 59<sup>e</sup> congrès de la JASTS à Osaka. Au-delà de la chance de voyager, c’est l’opportunité pour moi de présenter mes 
+      premiers travaux de neurosciences « pures ». C’est-à-dire incluant de belles images de cerveau 
       (<a href="https://www.sciencedirect.com/science/article/abs/pii/S0010027707002053?via%3Dihub">attention aux biais</a>).  
+      <br><br>
       Le poster dans son ensemble est consultable 
-      <a href="https://drive.google.com/file/d/1oH6xeO38jlgjcyndYTl1tHBvIRZNTaB-/view?usp=drive_link">ici</a>.
+      <a href="https://drive.google.com/file/d/1oH6xeO38jlgjcyndYTl1tHBvIRZNTaB-/view?usp=drive_link">ici</a>. Encore merci 
+      aux co-auteur·e·s M. Kato, S. Furukawa, S. Watanabe, M. Okamoto et K. Touhara. Mais surtout, merci à l’Université de Tokyo 
+      pour le soutien financier !
       </p>
       
-
       <p>
-      Ceci s'accompagne d'un changement majeur, avec le passage de ce site internet en français. Peut-être le signe
-      d'une fin d'expérience internationale, et d'un retour en Europe francophone ? Affaire à suivre.
+      Ceci s’accompagne d’un changement majeur : le passage de ce site internet en français. Peut-être le signe
+      d’une fin d’expérience internationale et d’un retour en Europe francophone ? Affaire à suivre…
       </p>
     `
   },
-{
+  {
     date: `2025-07-29`,
     title: `Lancement de la section "Actualités" !`,
     image: "img/news/Breaking_News_Alert.png",
     content: `
-      <p>Nouvelle section sur ce site !  
-      J'y publierai des actualités liées à la science : recherche, clinique, enseignement, congrès, projets, événements sociaux, etc.  
-      J'en ai aussi profité pour rafraîchir le site, notamment la présentation de la page d'accueil et la mise en place d'un  
-      <a href="https://matthieugg.github.io/resume.html">lecteur PDF dynamique</a> pour mon CV (mis à jour).</p>
+      <p>
+      Nouvelle section sur ce site !  
+      J’y publierai des actualités liées à la science : recherche, clinique, enseignement, congrès, projets, événements sociaux, etc.  
+      J’en ai aussi profité pour rafraîchir le site, notamment la présentation de la page d’accueil et la mise en place d’un  
+      <a href="https://matthieugg.github.io/resume.html">lecteur PDF dynamique</a> pour mon CV (mis à jour).
+      </p>
 
-      <p>Pas de "likes", pas de réactions, pas de commentaires — juste une diffusion d'information volontairement ciblée.  
-      Assez peu pour qu'aucune newsletter ne soit nécessaire, du moins pour le moment.  
-      Inspiré par le <a href="https://calnewport.com/writing/">minimalisme numérique</a>, avec du texte et peu de médias.  
+      <p>
+      Pas de "likes", pas de réactions, pas de commentaires — juste une diffusion d’informations volontairement ciblée.  
+      Trop peu pour qu’une newsletter fasse sens, du moins pour le moment.  
+      Inspiré par le <em><a href="https://calnewport.com/writing/">digital minimalism</a></em>, avec du texte et peu de médias.  
       Un bon début pour un internet décentralisé — en utilisant moins  
       <a href="https://linkedin.com/in/matthieu-gallou-guyot">LinkedIn</a> et  
-      <a href="https://jp.linkedin.com/in/matthieu-gallou-guyot">ResearchGate</a>, peut-être ?</p>
+      <a href="https://jp.linkedin.com/in/matthieu-gallou-guyot">ResearchGate</a>, peut-être ?
+      </p>
     `
-}
+  }
 ];
